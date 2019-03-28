0fc5310d8d9bda0faf1205224d334ad16ae24409
howru <42784871+H0WrU@users.noreply.github.com>
2019-03-28 15:36:41 -0700

    Update main.c
    added new program all of it.

--- main.c
+++ main.c
@@ -1,55 +1,55 @@
 #include <kipr/botball.h>
-int rm=0;
-int lm=3;
-int claw=1;
-int arm=0;
-int back=3;
-int claw_close=700;
-int arm_up=915; 
-int back_down=1112;
-int back_up=111;
+int rm=0;//right motor
+int lm=3;//left motor
+int rs=1000;//right motor speed
+int ls=1000;//left motor speed
+int claw=1;//claw that open and closes
+int arm=0;//arm that claw attaches to
+int back=3;//arm in the back attached to mini servo
+int claw_close=1200; //claw commands for puff balls
+int claw_open=2000; 
+int arm_up=915; //up position for arm
+int back_down=1000;//when arm goes down to attach to water unit
+int back_up=111; //back arm up 
 void counter(int dis);
 void starting_position();
+void down_slow(int arm_position);
 int main()
 {
     printf("Hello World\n");
     enable_servos();
-    //starting positions
-   starting_position();
+    //starting position
+    starting_position(); 
     
-    //outward from startbox
-    mrp(rm,-1000,-1300);
-    mrp(lm,-1000,-1300);
-    bmd(rm);
-    bmd(lm);
+    //back up out of start box and get unit
+    mav(rm,-rs);
+    mav(lm,-ls);
+    msleep(718);
     
-    //grab water reclamation unit
+   //back mini servo down to get unit
     set_servo_position(back,back_down);
     msleep(400);
     
-    
-    // forward then turn after water reclamation 
-    counter(1500);
-
-    mrp(rm,1000,1300);
-    mrp(lm,-1000,-1300);
-    bmd(rm);
-    bmd(lm);
-    
-    //go foward till black
-    while(analog(0)>3000)
+  //back up till black line
+    while(analog(0) < 1500)
     {
-        mrp(rm,1000,1300);
-        mrp(lm,1000,1300);
+        mav(rm,-rs);
+        mav(lm,-ls);
     }
-    bmd(rm);
-    bmd(lm);
+    ao();
+    
+    //turn after grabbing unit
+    mav(rm,-rs);
+    mav(lm,ls);
+    msleep(1300);
     
     
-    disable_servos();
+    
+
+    disable_servos(); 
     return 0;
 }
-//starting positions
+//starting positions 
 void starting_position()
 {
     set_servo_position(arm,arm_up);
@@ -64,10 +64,17 @@ void counter(int dis)
     clear_motor_position_counter(rm);
     while(get_motor_position_counter(rm)<dis)
     {
-    mrp(rm,1000,3000);
-    mrp(lm,1000,3000);
+    mav(rm,rs);
+    mav(lm,ls);
     }
-    bmd(rm);
-    bmd(lm);
 }
+void down_slow(int arm_position)
+{
 
+    while(get_servo_position(arm) < arm_position)
+
+    {
+        set_servo_position(arm,get_servo_position(arm) +10);
+        msleep(15);
+    }
+	}
